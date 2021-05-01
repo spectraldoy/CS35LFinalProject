03492dc0a4732c974674abed9ed48e0823e507e4
spectraldoy <littleguyisfum@gmail.com>
2021-05-02 01:50:50 +0530

    started dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// TODO: Make this all part of a dashboard class, with search bar and stuff all there
+// TODO: a header component, with logo and search bar
+// TODO: a lagoon component with all the links to click to browse schemes, etc.
+// TODO: a recent schemes thing with label dashboard
+// TODO: an account thing at the bottom
+
+class Header extends React.Component {
+  renderSearchBar() {
+    return (
+      <div className="App-search">
+        <input placeholder="Search schemes..."></input>
+      </div>
+    );
+  }
+
+  renderBrand() {
+    const name = "SchemePotato";
+    return (
+      <div className="App-brand">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {name}
+        </p>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <header className="App-header">
+        {this.renderBrand()}
+        {this.renderSearchBar()}
+      </header>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
