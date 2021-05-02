ddad627b75913da049319e8cf4578f3be04f614c
spectraldoy <littleguyisfum@gmail.com>
2021-05-02 18:58:17 +0530

    searchbar functional

--- src/dashboard.js
+++ src/dashboard.js
@@ -7,13 +7,6 @@ import React from 'react';
 // TODO: an account thing at the bottom
 
 class Header extends React.Component {
-  renderSearchBar() {
-    return (
-      <div className="App-search">
-        <input placeholder="Search schemes..."></input>
-      </div>
-    );
-  }
 
   renderBrand() {
     const name = "SchemePotato";
@@ -31,16 +24,69 @@ class Header extends React.Component {
     return (
       <header className="App-header">
         {this.renderBrand()}
-        {this.renderSearchBar()}
+        {this.props.renderSearchBar()}
       </header>
     );
   }
 }
 
+class Lagoon extends React.Component {
+  render() {
+    return 3;
+  }
+}
+
 class Dashboard extends React.Component {
-    render() {
-        return (<Header />);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+    };
+
+    this.submitQuery = this.submitQuery.bind(this);
+    this.displayQuery = this.displayQuery.bind(this);
+  }
+
+  submitQuery(event) {
+    // TODO: search database using this.state.searchQuery
+    event.preventDefault();
+    alert(this.state.searchQuery);
+
+    // clear out input
+    this.setState({
+      searchQuery: ""
+    });
+  }
+
+  displayQuery(event) {
+    // get input from search bar
+    this.setState({
+      searchQuery: event.target.value,
+    });
+  }
+
+  renderSearchBar() {
+    return (
+      // &nbsp is a non breaking space for a cleaner look
+      <form className="App-search" onSubmit={this.submitQuery}>
+        <label>
+          <input 
+            type="text"
+            placeholder="Search schemes..." 
+            value={this.state.searchQuery}
+            onChange={this.displayQuery}
+          />
+        </label>&nbsp;
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
+  render() {
+      return (
+        <Header renderSearchBar={this.renderSearchBar.bind(this)} />
+      );
+  }
 }
 
 export default Dashboard;
