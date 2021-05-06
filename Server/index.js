a3ac28e389636b2f2e75774926cae9030a7ec6c6
aaisara12 <aisara121201@gmail.com>
2021-05-06 13:41:26 -0700

    Server can receive and echo information about scheme data sent in JSON

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,34 @@ const PORT = process.env.PORT || 3001;  // Our port defaults to port 3001 (if no
 
 const app = express();
 
+app.use(express.json());
+
 // Here, we're setting up the callback function for when a client sends a get request at the /api endpoint.
 // Remember that an API endpoint is simply a "location" at which a client can send requests to the server.
 // I believe the callback function has some kind of signature like   void getCallback(Request& req, Response& res)
 // It appears as if this callback function's purpose is to modify the GET Response object before it is sent to the client
 app.get("/api", (req, res) => 
 {
-    res.json({ message: "Hello from server"});
+    res.send("Hello there!");
 });
 
-app.get("/home", (a, b) =>
+app.get("/home", (req, res) =>
 {
     b.json({ message: "This is supposed to be the home page!"});
+});
+
+// This does not store anything on a database yet -- it simply lets us see the contents of our JSON
+app.post("/create_scheme", (req, res) =>
+{
+    console.log("Received scheme \"" + req.body.name + "\"");
+    let cat_names = "";
+    for(var i = 0; i < req.body.categories.length; i++)
+    {
+        cat_names += req.body.categories[i].name;
+        if(i != req.body.categories.length - 1)
+            cat_names += ", ";
+    }
+    res.send(`Created scheme "${req.body.name}" with categories: ${cat_names}`);
 });
 
 
