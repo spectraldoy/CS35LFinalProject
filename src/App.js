0ede96c40ee3e533f31ec6aa1175e68ebd66bb6a
Eric Gan <egan1308@gmail.com>
2021-05-19 17:16:04 -0700

    Updated interface to server

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ function App() {
   );
 }
 
+// Custom hook. Essentially, when you submit login info, this will change its state, 
+// causing a re-render.
 function useToken() {
   const getToken = () => {
     const tokenString = sessionStorage.getItem('token');
