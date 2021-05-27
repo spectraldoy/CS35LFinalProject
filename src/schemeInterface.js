cc1d2d93c65c5645ce2d231ba9a019ec31fd9020
Zehua Tan <zehuatan18@gmail.com>
2021-05-27 00:07:16 -0500

    added checks to scheme interface before submitting

--- src/schemeInterface.js
+++ src/schemeInterface.js
@@ -1,80 +1,140 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
-import { render } from 'react-dom/cjs/react-dom.development';
 
 class schemeInterface extends React.Component {
-    
-    componentDidMount() {
-        console.log('Component Mounted');
-    }
-
-    render() {
-        console.log('Render Called')
-        return(
-            <div>
-                <Scheme></Scheme>
-            </div>
-        );
-    }
-
-}
-
-class Scheme extends React.Component {
     //Used as a key for categories
     categoriesCreated = 0;
     
-    //May be better to not have button in categories, and instead use the map() function when rendering
-    //May be better to use controlled components instead of DOM
     constructor(props) {
         super(props);
         this.state = {
             categories: [],
-            ids: []
+            ids: [],
+            university: '',
+            course: '',
+            professor: '',
         };
+
+        this.handleChange = this.handleChange.bind(this);
     }
-
-    postScheme() {
-        var university = document.getElementById('University').value;
-        var professor = document.getElementById('Professor').value;
-        var course = document.getElementById('Course').value;
-        var categoryNames = document.getElementsByClassName('Name');
-        var categoryWeights = document.getElementsByClassName('Weight');
-        var categories = [];
-        for (var i = 0; i < categoryNames.length; i++)
+    
+    handleChange(event) {
+        switch (event.target.name)
         {
-            categories.push({name:categoryNames[i].value, weight:categoryWeights[i].value});
+            case 'University':
+                this.setState({university: event.target.value});
+                break;
+            case 'Course':
+                this.setState({course: event.target.value});
+                break;
+            case 'Professor':
+                this.setState({professor: event.target.value});
+                break;
+            case 'Name':
+                var i = 0;
+                while (i < this.state.ids.length)
+                {
+                    if (this.state.ids[i] === parseInt(event.target.className))
+                    {
+                        break;
+                    }
+                    i++
+                }
+                var newCategories = this.state.categories.slice();
+                newCategories.splice(i, 1, {name:event.target.value, weight:this.state.categories[i]['weight']});
+                this.setState({categories:newCategories});
+                break;
+            case 'Weight':
+                var i = 0;
+                while (i < this.state.ids.length)
+                {
+                    if (this.state.ids[i] === parseInt(event.target.className))
+                    {
+                        break;
+                    }
+                    i++
+                }
+                var newCategories = this.state.categories.slice();
+                newCategories.splice(i, 1, {name:this.state.categories[i]['name'], weight:event.target.value});
+                this.setState({categories:newCategories});
+                break;
+            default:
+                break;
         }
+    }
+    
 
+    postScheme() {      
+        var sum = 0;
+        var valid = true;
+        var notEmpty = true;
+        var hasCategories = true;
+        var sumTo100 = true;
+        var error = false;
+        for (var i = 0; i < this.state.categories.length; i++)
+        {
+            if (this.state.categories[i]['weight'] === '' || this.state.categories[i]['name'] === '')
+            {
+                notEmpty = false;
+            }
+            if (parseInt(this.state.categories[i]['weight']) <= 0)
+            {
+                valid = false;
+            }
+            sum += parseInt(this.state.categories[i]['weight']);
+        }
+        if ((this.state.university === '' || this.state.course === '' || this.state.professor === '' || !notEmpty) && !error)
+        {
+            notEmpty = false;
+            error = true;
+            alert('Please fill out all fields before submitting!');
+        }
+        if (this.state.categories.length === 0 && !error)
+        {
+            hasCategories = false;
+            error = true;
+            alert('Must have at least one category!');
+        }
+        if (!valid && !error)
+        {
+            error = true;
+            alert('Invalid inputs!');
+        }
+        if (sum !== 100 & !error)
+        {
+            sumTo100 = false;
+            error = true;
+            alert('Weights must add up to 100!');
+        }
+        if (valid && notEmpty && hasCategories && sumTo100){
         const scheme = { 
-            university:university,
-            professor:professor,
-            class:course,
-            categories: categories
-         }
-         console.log(JSON.stringify(scheme));
-         console.log('POST Called')
+            owner:sessionStorage.getItem('user').split(',')[0],
+            university:this.state.university,
+            professor:this.state.professor,
+            class:this.state.course,
+            categories:this.state.categories
+        }
+         //console.log(JSON.stringify(scheme));
          axios.post('http://localhost:3001/grading_schemes', scheme)
          .catch(error => {
             this.setState({ errorMessage: error.message });
             console.error('There was an error!', error);
         });
+        alert('Submitted grading scheme!');
+    }
     }
 
     addCategory() {
         var num = this.categoriesCreated++;
         this.setState(() => ({
-            categories: [...this.state.categories, <Category key={'Category' + num}></Category>, 
-            <> <button type="button" key={'Button' + num} onClick={() => {this.removeCategory(num)}}>
-            Delete Category
-            </button><br></br> </>],
-            ids: [...this.state.ids, num, num]
-        })); 
+            categories: [...this.state.categories, {name:'', weight:0}],
+            ids: [...this.state.ids, num]
+        }));
     }
 
     //Go through all ids and find the corresponding index that matches. Remove the Category and Id that matches.
     removeCategory(id) {
-        
         var i = 0;
         while ( i < this.state.ids.length)
         {
@@ -85,9 +145,9 @@ class Scheme extends React.Component {
             i++
         }
         var newCategories = this.state.categories.slice();
-        newCategories.splice(i, 2);
+        newCategories.splice(i, 1);
         var newIds = this.state.ids.slice();
-        newIds.splice(i, 2);
+        newIds.splice(i, 1);
         
         this.setState(() => ({
             categories:newCategories,
@@ -110,23 +170,31 @@ class Scheme extends React.Component {
                     <div>
                     </div>
                     <form>
-                        <label htmlFor="University">University</label><br></br>
-                        <input type="text" id="University" name="University"></input><br></br>
-                        <label htmlFor="Course">Course</label><br></br>
-                        <input type="text" id="Course" name="Course"></input><br></br>
-                        <label htmlFor="Professor">Professor</label><br></br>
-                        <input type="text" id="Professor" name="Professor"></input><br></br>
-                        Categories:<br></br>
-                        {this.state.categories}
+                        <label htmlFor='University'>University</label><br></br>
+                        <input type='text' value={this.state.university} id='University' name='University' onChange={this.handleChange}></input><br></br>
+                        <label htmlFor='Course'>Course</label><br></br>
+                        <input type='text' value={this.state.course} id='Course' name='Course' onChange={this.handleChange}></input><br></br>
+                        <label htmlFor='Professor'>Professor</label><br></br>
+                        <input type='text' value={this.state.professor} id='Professor' name='Professor' onChange={this.handleChange}></input><br></br>
+                        Categories<br></br>
+                        {this.state.categories.map((element, index) => {
+                        return(
+                        <>
+                        <Category onChange={this.handleChange} inputName={element['name']} inputWeight={element['weight']} key={'Category' + this.state.ids[index]} name={this.state.ids[index]}></Category>
+                         <button type='button' key={'Button' + this.state.ids[index]} name={'Button' + this.state.ids[index]} onClick={() => {this.removeCategory(this.state.ids[index])}}>
+                        Delete Category
+                        </button><br></br> </>
+                        )
+                        })}
                     </form>
-                    
-                    <button type="button" onClick={() => {this.addCategory()}}>
+
+                    <button type='button' onClick={() => {this.addCategory()}}>
                     Add Category
                     </button>
-                    <button type="button" onClick={() => {this.reset()}}>
+                    <button type='button' onClick={() => {this.reset()}}>
                     Reset
                     </button>
-                    <button type="button" onClick={() => {this.postScheme()}}>
+                    <button type='button' onClick={() => {this.postScheme()}}>
                     Upload Scheme
                     </button>
                 </h1>
@@ -137,29 +205,19 @@ class Scheme extends React.Component {
 
 class Category extends React.Component {
     constructor(props) {
-        super(props);
-        
+        super(props);        
     }
 
-    componentDidMount() {
-        
-    }
-
-    componentWillUnmount() {
-
-    }
-    
     render() {
         return(
             <>
             <label>Name </label><br></br>
-            <input type="text" className="Name" id="Name" name="Name"></input><br></br>
+            <input defaultValue={this.props.inputName} type='text' className={this.props.name} name='Name' onChange={this.props.onChange}></input><br></br>
             <label>Weight </label><br></br>
-            <input type="number" className="Weight" id="Weight" name="Weight" min='0' max='100'></input><br></br>
+            <input defaultValue={this.props.inputValue} type='number' className={this.props.name} name='Weight' onChange={this.props.onChange}></input><br></br>
             </>
         );
     }
 }
-
 
 export default schemeInterface
